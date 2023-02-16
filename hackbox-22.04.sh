f1f09e04b56c614572713664adf91d4369cc846e
shiNZ0n <120986460+sh1nz0n@users.noreply.github.com>
2023-02-16 21:07:19 +0100

    Update hackbox-22.04.sh

--- hackbox-22.04.sh
+++ hackbox-22.04.sh
@@ -6,13 +6,24 @@ mkdir $tmpdir
 cd $tmpdir
 
 # install basic enumeration tools
-apt -y install nmap sqlmap dnsenum dnsmap dnsrecon ffuf gobuster
+apt -y install nmap sqlmap dnsenum dnsmap dnsrecon ffuf gobuster dirb
 
-# install basic password cracking tools
-apt -y install john hydra
+# install basic password cracking & brute forcing tools
+apt -y install john hydra medusa hashcat
 
 # install seclists
 wget https://github.com/danielmiessler/SecLists/archive/refs/tags/2022.4.tar.gz
 tar xzvf ./2022.4.tar.gz
 mv ./SecLists-2022.4 /usr/share/seclists
+
+# install metasploit
+wget https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb
+chmod +x msfupdate.erb
+./msfupdate.erb
+
+# install mimikatz 2.2.0-20220919
+wget https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20220919/mimikatz_trunk.zip
+mkdir -p /root/tools/mimikatz && cd /root/tools/mimikatz && unzip $tmpdir/mimikatz_trunk.zip
+mkdir -p /etc/skel/tools/mimikatz && unzip $tmpdir/mimikatz_trunk.zip
+cd $tmpdir
 
