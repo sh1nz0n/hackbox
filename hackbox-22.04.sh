9c83fc517ac7a6ad87ada9898ce2d9f115c6fbeb
shiNZ0n <120986460+sh1nz0n@users.noreply.github.com>
2023-02-16 22:52:30 +0100

    Update hackbox-22.04.sh

--- hackbox-22.04.sh
+++ hackbox-22.04.sh
@@ -35,7 +35,8 @@ cd $tmpdir
 # install burp suite community edition 2023.1.2
 curl -o burpsuite_community_linux_v2023_1_2.sh 'https://portswigger-cdn.net/burp/releases/download?product=community&version=2023.1.2&type=Linux' \
   && chmod +x burpsuite_community_linux_v2023_1_2.sh \
-  && ./burpsuite_community_linux_v2023_1_2.sh
+  && ln -sf $tmpdir/burpsuite_community_linux_v2023_1_2.sh /etc/skel/tools/ \
+  && ln -sf $tmpdir/burpsuite_community_linux_v2023_1_2.sh /root/tools/ \
   
 cd $tmpdir
 
